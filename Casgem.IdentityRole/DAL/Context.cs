33780e431def50a82a65ca4bceeef13c1f1e6a21
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-01 14:54:17 +0300

    Add project files.

---
+++ Casgem.IdentityRole/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Casgem.IdentityRole.DAL
+{
+	public class Context : IdentityDbContext<AppUser, AppRole, int>
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server = DESKTOP-13123BI; Initial Catalog = CasgemDbRole; Integrated Security = true;");
+		}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Casgem.IdentityRole/DAL/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Casgem.IdentityRole.DAL
+{
+	public class Product
+	{
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
